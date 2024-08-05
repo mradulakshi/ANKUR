60a8ff583e56870d11bb0abed494b1d9004f7d41
Saurav Singh Negi <140614521+SAURAV0703@users.noreply.github.com>
2024-08-05 08:07:56 +0530

    Update script_product.js

--- script_product.js
+++ script_product.js
@@ -1,24 +1,15 @@
-// Get the slideshow container and slides
 const slideshowContainer = document.querySelector('.products-slideshow-container');
 const slides = document.querySelectorAll('.mySlides');
 
-// Set the current slide index
 let currentSlideIndex = 0;
-
-// Function to show the next slide
 function showNextSlide() {
-  // Hide the current slide
 slides[currentSlideIndex].classList.remove('fade');
-
-  // Increment the slide index
+  
 currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-
-  // Show the next slide
 slides[currentSlideIndex].classList.add('fade');
 }
 
-// Set the slideshow interval
-setInterval(showNextSlide, 3000); // change the interval time to your liking
+setInterval(showNextSlide, 3000); 
 
-/******************************************************************** */
+
 
