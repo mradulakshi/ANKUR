60a5af7a18395d6534a4bbbb2ba043ae3a8f78ed
Saurav Singh Negi <140614521+SAURAV0703@users.noreply.github.com>
2024-08-05 09:27:35 +0530

    Update script_product.js

--- script_product.js
+++ script_product.js
@@ -4,12 +4,20 @@ const slides = document.querySelectorAll('.mySlides');
 let currentSlideIndex = 0;
 function showNextSlide() {
 slides[currentSlideIndex].classList.remove('fade');
-  
+
 currentSlideIndex = (currentSlideIndex + 1) % slides.length;
 slides[currentSlideIndex].classList.add('fade');
 }
 
 setInterval(showNextSlide, 3000); 
 
+// Get all wishlist buttons
+const wishlistBtns = document.querySelectorAll('.wishlist-btn');
 
-
+// Add event listener to each wishlist button
+wishlistBtns.forEach((btn) => {
+btn.addEventListener('click', () => {
+    // Toggle the 'active' class on the button
+    btn.classList.toggle('active');
+});
+});
